01909dbb8bb4012e86fab34ce4267d4ff5510772
Kieffer <kganza@traxiontech.net>
2023-03-29 09:09:52 +0800

    added featureTest

--- app/Http/Requests/StoreTaskRequest.php
+++ app/Http/Requests/StoreTaskRequest.php
@@ -26,7 +26,7 @@ class StoreTaskRequest extends FormRequest
         return [
             'title' => 'required|max:20',
             'description' => 'required|max:255',
-            'due_date' => 'required|date|after:today'
+            'due_date' => 'required|date'
         ];
     }
 }
